937265d32d27fd6ee993fcf4d4a032c63a061b6f
Zwl19940224 <919869469@qq.com>
2019-07-02 17:47:57 +0800

    '配置修改'

--- config/index.js
+++ config/index.js
@@ -36,7 +36,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: config.HOST, // can be overwritten by process.env.HOST
+    host: config._HOST, // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  HOST:'http://192.168.123.42'
+  HOST:'http://192.168.123.42',
+  _HOST:'192.168.123.42'
 }
